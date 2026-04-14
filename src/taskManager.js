3950035e6cd2487d803e16804783e688a94a7d1c
Júlia de Souza <juliadsouz2@gmail.com>
2026-04-14 20:23:29 -0300

    green: implementa countTasks,countCompleted, countPending

--- src/taskManager.js
+++ src/taskManager.js
@@ -32,3 +32,15 @@ export function filterTask(tasks,status) {
         return [...tasks];
   }
 }
+
+export function countTasks(tasks) {
+ return tasks.length;
+}
+
+export function countCompleted(tasks) {
+ return tasks.filter((task) => task.completed === true).length;
+}
+
+export function countPending(tasks) {
+  return tasks.filter((task) => task.completed === false).length;
+}

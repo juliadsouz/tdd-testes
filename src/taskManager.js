32218649dff5b6113ae2bb5d1860011fa58bac57
Júlia de Souza <juliadsouz2@gmail.com>
2026-04-14 21:19:52 -0300

    green: implementa modificações no createTask, validatePriority e filterByPriority

--- src/taskManager.js
+++ src/taskManager.js
@@ -3,14 +3,7 @@ let _nextId = 1;
 export function resetId() {
   _nextId = 1;
 }
-
-export function createTask(title) {
-  return {
-    id: _nextId++,
-    title: title.trim(),
-    completed: false,
-  };
-}
+//Exercício 1 - RemoveTask
 
 export function addTask(tasks, title) {
   const newTask = createTask(title);
@@ -20,6 +13,8 @@ export function addTask(tasks, title) {
 
 export function removeTask(tasks, taskId) {
   return tasks.filter((task) => task.id !== taskId); }
+
+//Exercício 2 - FilterTask
 
 export function filterTask(tasks,status) {
   switch(status) {
@@ -33,6 +28,8 @@ export function filterTask(tasks,status) {
   }
 }
 
+//Exercício 3 - Contagens
+
 export function countTasks(tasks) {
  return tasks.length;
 }
@@ -44,3 +41,26 @@ export function countCompleted(tasks) {
 export function countPending(tasks) {
   return tasks.filter((task) => task.completed === false).length;
 }
+
+//Exercício 4 - Priority, ValidatePriority, filterByPriority
+
+export function createTask(title, priority) {
+  if (!['low', 'medium', 'high'].includes(priority)) {
+    priority = 'medium';
+  }
+  return {
+    id: _nextId++,
+    title: title.trim(),
+    priority: priority,
+    completed: false,
+    
+  };
+}
+
+export function filterByPriority(tasks, priority) {
+  return tasks.filter(t => t.priority === priority);
+}
+
+export function validatePriority(priority) {
+    return ['low', 'medium', 'high'].includes(priority);
+  }
